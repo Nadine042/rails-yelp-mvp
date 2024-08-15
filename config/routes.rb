a247f69af3f183419c671dc49456259527fa0c1e
Nadine Werner <nadine-werner@teleos-web.de>
2024-08-15 10:25:10 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
